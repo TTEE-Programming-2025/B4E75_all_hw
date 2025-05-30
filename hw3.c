1fd2604229c49c3907fd2fbb6a7d8186cadda2d7
kuo-po-lin <u11002275@o365.ttu.edu.tw>
2025-05-30 15:34:25 +0800

    試發

---
+++ hw3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h> // For getch()
+#include <time.h>
+#include <string.h>
+
+#define SIZE 9
+#define MAX_RESERVED 10 
+#define PASSWORD 2025
+
+char seats[SIZE][SIZE];
+char tempSeats[SIZE][SIZE];
