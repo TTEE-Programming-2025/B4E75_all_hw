53ae34e6901de3d702222b6ccd6dfa5b2819806d
kuo-po-lin <u11002275@o365.ttu.edu.tw>
2025-05-30 15:35:57 +0800

    尋找問題

--- hw3.c
+++ hw3.c
@@ -10,3 +10,35 @@
 
 char seats[SIZE][SIZE];
 char tempSeats[SIZE][SIZE];
+// Initialise seats and tag 10 random seats as '*'
+void iniSeats() {
+    for (int i = 0; i < SIZE; i++)
+       for (int j = 0; j < SIZE; j++)
+          seats[i][j]='-';
+
+    int count = 0;
+    while (count < MAX_RESERVED) {
+        int r = rand() %SIZE;
+        int c = rand() %SIZE;
+        if(seats[r][c] != '*') {
+           seats[r][c] = '*';
+           count++;
+        }
+    }
+}
+
+// Showing the seats 
+void showSeats() {
+    printf("\\123456789\n");
+    for (int i = SIZE - 1; i >= 0; i--) {
+        printf("%d", i + 1);
+        for (int j = 0; j < SIZE; j++) {
+            printf("%c", seats[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nPress any key to return to main menu");
+    getch();
+    system("cls");
+}
+
